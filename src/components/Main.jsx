56f8e425be22f7a352813fa8428d284aafe6dedb
Nontapat Toei - 6609612046 <andas4v4ge@gmail.com>
2026-03-03 20:47:15 +0700

    Add ternary for Get a recipe button

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -27,9 +27,19 @@ export default function Main() {
                  />
                  <button>Add Ingredients</button>
             </form>
-            <ul>
-                {ingredientLists}
-            </ul>
+            {ingredient.length > 0 ?
+          <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientLists}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section> : <h2>No ingredients added yet.</h2>
+            }
        
         </main>
     )
